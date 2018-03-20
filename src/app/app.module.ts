98810f8180b5655669872dea02f125222e0c4a79
Gustavo Santiago <gustavosantig1@gmail.com>
2018-03-19 22:02:33 -0300

    basic settings for reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 // angular library
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 // app components
@@ -51,7 +51,8 @@ import * as $ from 'jquery';
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryTaskDataService)
+    InMemoryWebApiModule.forRoot(InMemoryTaskDataService),
+    ReactiveFormsModule
   ],
   providers: [
     TaskService

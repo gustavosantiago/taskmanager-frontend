4893e5e52f51a436723ef09e66ff6cd5d8d58aab
Gustavo Santiago <gustavo.santiago@fortaleza.ce.gov.br>
2018-04-03 13:54:52 -0300

    improvment on sign-in

--- src/app/sign-in-form/sign-in-form.component.ts
+++ src/app/sign-in-form/sign-in-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { AuthService } from '../shared/auth.service';
 import { FormUtils } from './../shared/form.utils';
 
 @Component({
@@ -12,17 +14,42 @@ import { FormUtils } from './../shared/form.utils';
 export class SignInFormComponent {
   public form: FormGroup;
   public formUtils: FormUtils;
+  public submitted: boolean;
+  public formErrors: Array<string>;
 
-  public constructor(private formBuilder: FormBuilder) {
+  public constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) {
+    this.signInParams();
+    this.formUtils  = new FormUtils(this.form);
+    this.submitted  = false;
+    this.formErrors = null;
+  }
+
+  public signInUser() {
+    this.submitted = true;
+    let email    = this.form.get('email').value;
+    let password = this.form.get('password').value;
+    
+    this.authService.signIn(email, password)
+      .subscribe(
+        response => {
+          this.router.navigate(['/dashboard']);
+          this.formErrors = null;
+        },
+        error => {
+          this.submitted = false;
+
+          if(error.status=== 401)
+            this.formErrors = JSON.parse(error._body).errors;
+          else
+            this.formErrors = ['Não foi possível processar a solicitação']
+        }
+      )
+  }
+
+  private signInParams() {
     this.form = this.formBuilder.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required]]
     });
-
-    this.formUtils = new FormUtils(this.form);
-  }
-
-  public signInUser() {
-    console.log('valido');
   }
 }
